7c5397826ea1cb61f0ec628587e46cd937493465
Saikrishna41 <saai.krrishna@gmail.com>
2024-03-20 15:06:23 +0530

    add watercounter and wellness composable

---
+++ app/src/main/java/com/techfortyone/composestatescl/WaterCounter.kt
@@ -0,0 +1,14 @@
+package com.techfortyone.composestatescl
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCounter(modifier : Modifier = Modifier) {
+    val count = 0
+    Text(text = "You have had $$count glasses of water",
+        modifier = modifier.padding(16.dp))
+}
